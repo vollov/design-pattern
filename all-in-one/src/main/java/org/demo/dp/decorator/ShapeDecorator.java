481f76c0a7c8953fb20039325504ac413eda4245
dike.zhang@gmail.com <dike.zhang@gmail.com>
2016-02-20 16:58:54 -0500

    added facade

---
+++ all-in-one/src/main/java/org/demo/dp/decorator/ShapeDecorator.java
@@ -0,0 +1,15 @@
+package org.demo.dp.decorator;
+
+import org.demo.dp.util.Shape;
+
+public abstract class ShapeDecorator implements Shape {
+	protected Shape decoratedShape;
+
+	public ShapeDecorator(Shape decoratedShape) {
+		this.decoratedShape = decoratedShape;
+	}
+
+	public void draw() {
+		decoratedShape.draw();
+	}
+}
---
+++ all-in-one/src/main/java/org/demo/dp/util/Shape.java
@@ -0,0 +1,5 @@
+package org.demo.dp.util;
+
+public interface Shape {
+	void draw();
+}
